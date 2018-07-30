7307d814610dac3d27a46fab22b8a2fa2e24c8a9
alex farioletti <gsf.nyc@gmail.com>
2018-07-30 11:42:25 -0700

    puts ints

--- _printf.c
+++ _printf.c
@@ -42,6 +42,17 @@ int _printf(const char *format, ...)
 
 			switch (format[i])
 			{
+                case 'd':
+                case 'i':
+                    d++;
+                    /*
+                    *if (d > numDirects)
+                    *   return (-1);
+                    */
+                    retval = _putint(va_arg(arglist, int));
+                    i++;
+                    break;
+
 				case 'c':
 					d++;
 					/*
@@ -113,4 +124,44 @@ int _printf(const char *format, ...)
 
 
 
+/**
+ * _putint - print a int
+ * @num: int to print
+ *
+ * Return: Number of characters printed (excluding the null byte)
+*/
 
+int _putint(int num)
+{
+	int i, tmp, retval;
+
+	retval = 0;
+
+	tmp = num;
+	i = 1;
+	while (tmp/10)
+	{
+		tmp /= 10;
+		i *= 10;
+	}
+	while (i)
+	{
+		tmp = num / i;
+	    if (tmp < 0)
+		{
+			_putchar('-');
+			retval++;
+			tmp *= -1;
+			num = num % i * -1;
+		}
+		else
+		{
+			num = num % i;
+		}
+
+		_putchar('0' + tmp);
+		retval++;
+		i /= 10;
+	}
+	return (retval);
+}
