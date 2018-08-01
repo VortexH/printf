a77f3079121f096699c4526c17a911b2dd2b8ebb
VortexH <hemant.heer@gmail.com>
2018-08-01 00:26:54 +0000

    betty pass, output  good

--- printrevstring.c
+++ printrevstring.c
@@ -13,29 +13,17 @@
 int _printrevstring(va_list valist)
 {
 	char *string = va_arg(valist, char *);
-	int len, i;
+	int len, i = 0;
 
 	if (string)
 	{
-		len = _strlen(string);
-		i = 0;
-		while (i < len / 2)
+		len = _strlen(string) - 1;
+		while (len >= 0)
 		{
-			tmp = string[i];
-			string[i] = string[len - 1 - i];
-			string[len - 1 - i] = tmp;
-
-			i++;
-
-		}
-
-		i = 0;
-		while (string[i])
-		{
-			write(1, string[i], 1);
+			_putchar(string[len]);
+			len--;
 			i++;
 		}
-
 	}
 
 	return (i);
