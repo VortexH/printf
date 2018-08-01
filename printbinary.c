2613ae37891d11a0e84c20728a932d09461cde04
VortexH <hemant.heer@gmail.com>
2018-08-01 05:58:12 +0000

    print binary added to getfunc, initial implementatio of printbinary

--- printbinary.c
+++ printbinary.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdlib.h>
 
 /**
  * _printbinary - converts an unsigned int to binary given the conversion
@@ -10,15 +11,50 @@
  * Description: unsigned ints are converted to binary in the function
  *
  * Return: type int - number of characters printed
+*/
 
 int _printbinary(va_list valist)
 {
 	unsigned int num = va_arg(valist, unsigned int);
+	unsigned int rem = 0;
 	int retval = 0;
+	int len = 0;
+	int i = 0;
+	/* allocate well more than 32 bits worth of char bytes */
+	char *numarray = malloc(sizeof(char) * 60);
 
-	return (convint(num));
+	if (!numarray)
+		return (-1);
+
+	if (num == 0 || num == 1)
+	{
+		_putchar(num + '0');
+		retval++;
+	}
+	else
+	{
+		while (num != 0)
+		{
+			rem = num % 2;
+			num /= 2;
+			numarray[i] = rem + '0';
+			i++;
+		}
+
+		numarray[i] = '\0';
+		len = _strlen(numarray);
+		len -= 1;
+		/* print each byte to stdout in reverse */
+		while (len >= 0)
+		{
+			_putchar(numarray[len]);
+			retval++;
+			len--;
+		}
+
+	}
+
+	free(numarray);
+	return (retval);
+
 }
-
-int convint(unsigned int num)
-{
-*/
